2106aa66888e72b295cb6b04ea5d286b4fc2a85f
Aaron Lippold <lippold@gmail.com>
2017-06-27 17:18:39 -0400

    more little updates.
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- parser.rb
+++ parser.rb
@@ -4,13 +4,13 @@ require 'json'
 require 'pp'
 require 'nokogiri'
 
-# filename = 'exam.xml'
-# xml = File.read(filename)
-# doc = Nokogiri::XML(xml)
-# # ... make changes to doc ...
-# File.write(filename, doc.to_xml)
+  # filename = 'exam.xml'
+  # xml = File.read(filename)
+  # doc = Nokogiri::XML(xml)
+  # # ... make changes to doc ...
+  # File.write(filename, doc.to_xml)
 
-#  cat file2.json | jq '.profiles[].controls[] | "\(.impact) \(.tags.gid)"'
+  #  cat file2.json | jq '.profiles[].controls[] | "\(.impact) \(.tags.gid)"'
 
 json = File.read('file2.json')
 @norgi = Nokogiri::XML(File.open('test.ckl'))
@@ -21,24 +21,6 @@ json = File.read('file2.json')
 #   puts "#{vnumber}: is #{status}"
 # end
 
-def parse_inspec_json(json)
-  file = JSON.parse(json)
-  controls = file['profiles'][0]['controls']
-  data = {}
-
-  controls.each do |control|
-    gid = control['tags']['gid']
-    data[gid] = {}
-    data[gid]['impact'] = control['impact']
-    data[gid]['status'] = control.key?('results') ? control['results'][0]['status'] : 'nil'
-  end
-  data
-end
-
-myjson = parse_inspec_json(json)
-
-pp myjson['V-73015']['impact']
-
 def find_status_by_vuln(vuln)
   nodes = @norgi.search "[text()*='#{vuln}']"
   node = nodes.first
@@ -48,37 +30,36 @@ end
 def set_status_by_vuln(vuln,status)
   nodes = @norgi.search "[text()*='#{vuln}']"
   node = nodes.first
-  puts "I found node: #{node}"
   current_status = node.parent.parent.xpath('./STATUS')
-  puts "The node has content: #{node.content}"
-  puts "It has the status of: #{current_status}"
-  puts node.parent.parent.at('./STATUS').content = "test"
-  puts @norgi.to_xml
+  node.parent.parent.at('./STATUS').content = status
 end
 
-#find_status_by_vuln('V-7284')
-#set_status_by_vuln('V-7284','test')
-#find_status_by_vuln('V-7284')
+def inspec_status_to_clk_status(vuln, json_results)
+  puts 'Not A Finding' if json_results[vuln]['status'] == 'passed'
+  puts 'Finding' if json_results[vuln]['status'] == 'failed'
+  puts 'Not Evaluated(nil)' if json_results[vuln]['status'] == 'nil'
+  puts 'Not Evaluated' if json_results[vuln]['status'] == 'skipped'
+  puts 'Not Applicable' if json_results[vuln]['imapct'] == '0'
+end
 
-#   puts "x: #{x.xpath('./STATUS').text}"
-# end
-
-#xml.elements.each('CHECKLIST/STIGS/iSTIG/VULN/STIG_DATA/ATTRIBUTE_DATA') { |element| puts element.text if element.text =~ /^V-.*/ }
-
-  def map_controls(controls)
-    controls.each do |x|
-      if x['status'].match('passed')
-        puts "#{x['id']} should be set to 'Not A Finding'"
-      elsif x['status'].match('failed')
-        puts "#{x['id']} should be set to 'Finding'"
-      elsif x['status'].match('skipped')
-        puts "#{x['id']} should be set to 'Not Evaluated'"
-      # case where control impact == 0 => NotAppliciable
-      else x['status'].match('z')
-        puts "#{x['id']} should be set to 'z'"
-      end
-    end
+def parse_json(json)
+  file = JSON.parse(json)
+  controls = file['profiles'][0]['controls']
+  data = {}
+  controls.each do |control|
+    gid = control['tags']['gid']
+    data[gid] = {}
+    data[gid]['impact'] = control['impact']
+    data[gid]['status'] = control.key?('results') ? control['results'][0]['status'] : 'nil'
   end
+  data
+end
+
+test_results = parse_json(json)
+
+test_results.keys.each do |vuln|
+  inspec_status_to_clk_status(vuln, test_results)
+end
 
 # puts x = parse_inspec_json(json)
 # map_controls(x)
