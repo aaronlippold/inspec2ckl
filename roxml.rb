8a38bf0243e665eee1f5ce7f1ca1c1e2cab33e0f
Aaron Lippold <lippold@gmail.com>
2017-07-02 19:15:25 -0400

    ROXML is also working, now we have to see which one is easiest to get STIG_DATA/<a><b></STIG.. form
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- roxml.rb
+++ roxml.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/env ruby
 require 'roxml'
 
+# see: https://github.com/Empact/roxml
+
 class CHECKLIST
   include ROXML
   xml_accessor :type, from: 'ASSET_TYPE', in: 'ASSET'
   xml_accessor :status, from: 'status', in: 'ASSET/STIGS/iSTIG'
+  xml_accessor :id, from: 'ATTRIBUTE_DATA', in: 'ASSET/STIGS/iSTIG/VULN/STIG_DATA'
 end
 
 lib = CHECKLIST.from_xml(File.read('data/postgres_checklist.ckl'))
+
+puts lib.type
+puts lib.status
+puts lib.id
